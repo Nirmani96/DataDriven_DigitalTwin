00733f27b2f517282aa4c65d4dbae01375ab83fa
Nirmani96 <66180469+Nirmani96@users.noreply.github.com>
2026-04-22 22:14:47 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 """
 Created on Tue Jul 29 20:05:24 2025
 
-@author: 40464988
+@author: Nirmani Amarasinghe
 """
-
+# Change this date to run the digital twin from a different forecast point
 from datetime import datetime
 
 FORECAST_DATE = datetime(2007, 9, 20,12,0)
